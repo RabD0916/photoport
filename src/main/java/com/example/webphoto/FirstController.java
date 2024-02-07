28653c28ca32e711fae415da98f61a149fdebc12
Jah07 <wogjs6604@naver.com>
2024-02-07 11:40:24 +0900

    1차

--- src/main/java/com/example/webphoto/FirstController.java
+++ src/main/java/com/example/webphoto/FirstController.java
@@ -1,2 +1,15 @@
-package com.example.webphoto;public class FirstController {
+package com.example.webphoto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FirstController {
+    @GetMapping("/showMe")
+    public List<String> hello() {
+        return Arrays.asList("정재헌", "김영우");
+    }
 }
