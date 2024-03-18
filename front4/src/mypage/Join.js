d1d152b53402e1990244cff517f029bc61834f04
Jah07 <wogjs6604@naver.com>
2024-03-18 18:55:22 +0900

    회원가입 디자인 완료 세부 동작 기능 구현 못함

--- front4/src/mypage/Join.js
+++ front4/src/mypage/Join.js
@@ -4,14 +4,26 @@ const join=() =>{
     return(
         <div>
             <h2>회원가입</h2>
-            <section>
-                <h4>회원가입 약관동의 및 본인인증단계 입니다.</h4>
-                <h4>홈페이지 이용약관 동의 (필수)</h4>
-                <h4>개인정보 수집 및 이용동의 (필수)</h4>
-                <h4>광고성 정보 수신동의 (선택)</h4>
-                <h4>회원가입 유의사항</h4>
-                <Link to={"/signup"} className={"sign"}>다음</Link>
-                <br />
+            <section className="join_style">
+                <h3>회원가입 약관동의 및 본인인증단계 입니다.</h3>
+                <div className="all_agree"> 전체동의<input type="checkbox" /></div>
+                <h4 className="check_agree">*홈페이지 이용약관 동의 (필수)<span> 동의<input type="checkbox" /></span></h4>
+                <textarea name="" className="privacy_scroll">
+
+                </textarea>
+                <h4 className="check_agree">개인정보 수집 및 이용동의 (필수)<span> 동의<input type="checkbox" /></span></h4>
+                <textarea name="" className="privacy_scroll">
+
+                </textarea>
+                <h4 className="check_agree">광고성 정보 수신동의 (선택)<span> 동의<input type="checkbox" /></span></h4>
+                <textarea name="" className="privacy_scroll">
+
+                </textarea>
+                <h4 className="check_agree">회원가입 유의사항</h4>
+                <textarea name="" className="privacy_scroll">
+
+                </textarea>
+                <Link to={"/signup"} className={"next_button"}>다음</Link>
             </section>
         </div>
     )
--- front4/src/mypage/Signup.js
+++ front4/src/mypage/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./css/login.css";
 const Signup=() =>{
     const [selectedOption, setSelectedOption] = useState('naver.com'); // 기본값으로 naver.com을 설정
     const handleSelectChange = (event) => {
@@ -9,21 +10,16 @@ const Signup=() =>{
             <h1>회원가입</h1>
             <div className="join_style">
             <form action="" method="post">
-                <p className="sign_p">아이디 : </p><input type="text" className="sign_input"/>
-                <p className="sign_p">비밀번호 : </p><input type="password" className="sign_input"/>
-                <p className="sign_p">비밀번호 확인 : </p><input type="password" className="sign_input"/>
-                <p className="sign_p">이메일 : </p><p className="sign_input"><input type="text" className="call_input"/> @ <input type="text" className="call_input" placeholder=""/>
-                <select id="" >
-                    <option value="">이메일도메인</option>
-                    <option value="naver.com">naver.com</option>
-                    <option value="google.com">google.com</option>
-                    <option value="hanmail.com">hanmail.com</option>
-                    <option value="daum.com">daum.com</option>
-                </select>
+                <p className="sign_p">*아이디  </p><input type="text" className="sign_input" placeholder="아이디를 입력하세요."/>
+                <p className="sign_p">*비밀번호  </p><input type="password" className="sign_input" placeholder="비밀번호를 입력하세요."/>
+                <p className="sign_p">*비밀번호 확인  </p><input type="password" className="sign_input" placeholder="비밀번호 확인."/>
+                <p className="sign_p">*이메일  </p>
+                <p className="email_style">
+                <input type="text" className="sign_input" placeholder="이메일 입력"/>
             </p>
-                <p className="sign_p">전화번호 : </p><input type="text" className="sign_input"/>
+                <p className="sign_p">전화번호  </p><input type="text" className="sign_input"/>
             </form>
-            <input type="submit" value="회원가입"/>
+            <input className="next_input" type="submit" value="회원가입"/>
             </div>
         </div>
     )
