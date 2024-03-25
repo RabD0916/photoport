2997c5275cac0b15cbbf70139ae97f0b8b0b452e
Jah07 <wogjs6604@naver.com>
2024-03-25 10:37:06 +0900

    main nav footer 반응형 수정 및 main center2 유형 변경

--- front4/src/Component/Footer.js
+++ front4/src/Component/Footer.js
@@ -7,23 +7,22 @@ function Footer(){
         <div className="main_footer">
             <div className="footer_first">
                 <div className="footer_left">
-                    <div>이용약관 개인정보처리방침</div>
-                    <div>조이름: 카고</div>
-                    <div>조원 : 정재헌 ,김영우,신철규,권혁,유현수</div>
-                    <div>문의:wogjs@naver.com</div>
+                    <p>이용약관 개인정보처리방침</p>
+                    <p>조이름: 카고</p>
+                    <p>조원 : 정재헌 ,김영우,신철규,권혁,유현수</p>
+                    <p>문의:wogjs@naver.com</p>
                     <hr className="Line"/>
-                    <div>
-                        <img className="application" src={Mainlogo} alt="인생네컷어플"/>
-                    </div>
-
-                    <div>
+                    <div className={"application"}>
+                        <img className="app_logo" src={Mainlogo} alt="인생네컷어플"/>
                         <img className="google_button" src="/googleplay.png" alt="다운로드버튼"/>
                     </div>
-
                 </div>
                 <div className="footer_right">
                     <img className="naver_map" src="/map.jpg" alt="지도"/>
                     {/*<div className={"linkiss"}>링크들</div>*/}
+                </div>
+                <div className={"footer_right"}>
+
                 </div>
             </div>
 
--- front4/src/Component/Main.js
+++ front4/src/Component/Main.js
@@ -23,31 +23,26 @@ const Main = (props) => {
                 <img className="center1" src="/backbaxk.jpg" alt="센터1"/>
             </div>
             <div className="mainBar">
-                <li className="mainMenu left1"><Link to={"/Notice"} className={"mainnav"}>공지사항</Link></li>
-                <li className="mainMenu right1"><Link to={"/Event"} className={"mainnav"}>이벤트</Link></li>
+                <li className="mainMenu left1"><Link to={"/Notice"} className={"main_nav"}>공지사항</Link></li>
+                <li className="mainMenu right1"><Link to={"/Event"} className={"main_nav"}>이벤트</Link></li>
             </div>
 
-            <div className="mainfirst">
-                <div className="firstleft">
-                    <img src={Mainlogo} className={"mainlogo"} alt={"메인로고"}/>
+            <div className="main_first">
+                <div className="first_left">
+                    <img src={Mainlogo} className={"main_logo"} alt={"메인로고"}/>
                 </div>
-                <div className="firstright">간편하게 스캔하여 저장하자!! 네컷앨범을 사용해 보세요</div>
+                <p className="first_right">간편하게 스캔하여 저장하자!! 네컷앨범을 사용해 보세요</p>
             </div>
-            <div className="fontfont">요즘은 사진을 어떻게 찍을까?</div>
-            <div className="mainsecond">
-                <div className="second">
-                    <div className="second1" style={{display: slideIndex === 0 ? 'block' : 'none'}}>
-                        <img className="seimg" src="/one.jpg" alt="짱구 독사진 포즈"/>
-                    </div>
-                    <div className="second1" style={{display: slideIndex === 1 ? 'block' : 'none'}}>
-                        <img className="seimg" src="/one1.jpg" alt="수지 독사진 포즈"/>
-                    </div>
-                    <div className="second2">포즈2</div>
-                    <div className="second3">포즈3</div>
+            <div className="font_font">요즘은 사진을 어떻게 찍을까?</div>
+            <div className="main_div">
+                <div className="third">
+                    <img className={"frame"} src={"/one.jpg"} alt={"프레임1"}/>
+                    <img className={"frame"} src={"/one1.jpg"} alt={"프레임2"}/>
+                    <img className={"frame"} src={"/one1.jpg"} alt={"프레임3"}/>
                 </div>
             </div>
-            <div className="fontfont">원하는 프레임을 골라 자신만의 네컷을 만들자!</div>
-            <div className="mainthird">
+            <div className="font_font">원하는 프레임을 골라 자신만의 네컷을 만들자!</div>
+            <div className="main_div">
                 <div className="third">
                     <img className={"frame"} src={Frame1} alt={"프레임1"}/>
                     <img className={"frame"} src={Frame2} alt={"프레임2"}/>
